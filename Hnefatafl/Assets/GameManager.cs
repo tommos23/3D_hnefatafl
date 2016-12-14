c7e5e7d746dd351189e6d800a7d0d0ebbc2d924b
tommos23 <tommos23@gmail.com>
2016-12-14 15:51:10 +0000

    Basic Movement
    A piece can now be moved around the board in straight lines

---
+++ Hnefatafl/Assets/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public int gameState = 0;           // In this state, the code is waiting for : 0 = Piece selection, 1 = Piece animation, 2 = Player2/AI movement
+                                        //private int activePlayer = 0;		// 0 = Player1, 1 = Player2, 2 = AI, to be used later
+    private GameObject SelectedPiece;   // Selected Piece
+
+    //Update SlectedPiece with the GameObject inputted to this function
+    public void SelectPiece(GameObject _PieceToSelect)
+    {
+        // Change color of the selected piece to make it apparent. Put it back to white when the piece is unselected
+        if (SelectedPiece)
+        {
+            SelectedPiece.GetComponent<Renderer>().material.color = Color.white;
+        }
+        SelectedPiece = _PieceToSelect;
+        SelectedPiece.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    // Move the SelectedPiece to the inputted coords
+    public bool MovePiece(Vector3 _coordToMove)
+    {
+        bool move = false;
+        if(SelectedPiece.transform.position.x == _coordToMove.x)
+        {
+            if (SelectedPiece.transform.position.z != _coordToMove.z)
+            {
+                move = true;
+            }
+        }
+        else if (SelectedPiece.transform.position.z == _coordToMove.z)
+        {
+            move = true;
+        }
+        else
+        {
+            return false;
+        }
+
+        if(move)
+        {
+            SelectedPiece.transform.position = _coordToMove;// Move the piece
+            SelectedPiece.GetComponent<Renderer>().material.color = Color.white; // Change it's color back
+            SelectedPiece = null; // Unselect the Piece
+        }
+        return true;
+    }
+
+    // Change the state of the game
+    public void ChangeState(int _newState)
+    {
+        gameState = _newState;
+        Debug.Log("GameState = " + _newState);
+    }
+}
--- Hnefatafl/Assets/PlayerControls.cs
+++ Hnefatafl/Assets/PlayerControls.cs
@@ -1,16 +1,76 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControls : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private Camera PlayerCam;           // Camera used by the player
+    private GameManager _GameManager;   // GameObject responsible for the management of the game
+
+    // Use this for initialization
+    void Start () {
+        PlayerCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>(); // Find the Camera's GameObject from its tag 
+        _GameManager = gameObject.GetComponent<GameManager>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        // Look for Mouse Inputs
+        GetMouseInputs();
+    }
+
+    private void GetMouseInputs()
+    {
+        Ray _ray;
+        RaycastHit _hitInfo;
+
+        // Select a piece if the gameState is 0 or 1
+        if (_GameManager.gameState < 2)
+        {
+            // On Left Click
+            if (Input.GetMouseButtonDown(0))
+            {
+                _ray = PlayerCam.ScreenPointToRay(Input.mousePosition); // Specify the ray to be casted from the position of the mouse click
+
+                // Raycast and verify that it collided
+                if (Physics.Raycast(_ray, out _hitInfo))
+                {
+                    // Select the piece if it has the good Tag
+                    if (_hitInfo.collider.gameObject.tag == ("PiecePlayer1"))
+                    {
+                        _GameManager.SelectPiece(_hitInfo.collider.gameObject);
+                        _GameManager.ChangeState(1);
+                    }
+                }
+            }
+        }
+
+        // Move the piece if the gameState is 1
+        if (_GameManager.gameState == 1)
+        {
+            Vector3 selectedCoord;
+
+            // On Left Click
+            if (Input.GetMouseButtonDown(0))
+            {
+                _ray = PlayerCam.ScreenPointToRay(Input.mousePosition); // Specify the ray to be casted from the position of the mouse click
+
+                // Raycast and verify that it collided
+                if (Physics.Raycast(_ray, out _hitInfo))
+                {
+
+                    // Select the piece if it has the good Tag
+                    if (_hitInfo.collider.gameObject.tag == ("Cube"))
+                    {
+                        selectedCoord = new Vector3(_hitInfo.collider.gameObject.transform.position.x, 1, _hitInfo.collider.gameObject.transform.position.z);
+                        if (_GameManager.MovePiece(selectedCoord))
+                        {
+                            _GameManager.ChangeState(0);
+                        }                     
+                    }
+                }
+            }
+        }
+    }
 }
